30da391c30581152bc439073b0f818d717e7df0e
rgavrysh <grome365@gmail.com>
2018-03-21 21:31:28 +0100

    Replaced sout for logger. Enabled all actuator endpoints.

--- cloud-stream/src/main/java/org/spring/springkafka/service/ErrorListener.java
+++ cloud-stream/src/main/java/org/spring/springkafka/service/ErrorListener.java
@@ -1,5 +1,7 @@
 package org.spring.springkafka.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spring.springkafka.model.ErrorModel;
 import org.spring.springkafka.stream.ErrorStream;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -8,9 +10,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ErrorListener {
+    private final Logger logger = LoggerFactory.getLogger(ErrorListener.class);
 
     @StreamListener(ErrorStream.INPUT)
     public void logErrors(@Payload ErrorModel errorModel) {
-        System.out.println(errorModel.toString());
+        logger.info(errorModel.toString());
     }
 }
